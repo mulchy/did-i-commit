31f2d792a63a75916a7322be36377a002d771313
Ryan Mulcahy <ryan@primeacademy.io>
2016-08-29 02:39:22 -0500

    Mostly working.

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -7,36 +7,25 @@ defmodule DidICommit.AuthController do
     redirect conn, external: GitHub.authorize_url!
   end
 
-  def delete(conn, %{"id" => id}) do
-    auth = Repo.get!(Auth, id)
-
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(auth)
-
-    send_resp(conn, :no_content, "")
+  def delete(conn, _params) do
+    conn
+    |> put_flash(:info, "You have been logged out!")
+    |> configure_session(drop: true)
+    |> redirect(to: "/")
   end
 
   def callback(conn, %{"code" => code}) do
     client = GitHub.get_token!(code: code)
     user = get_user!(client)
-    notifications = get_notifications!(client)
     
     conn
     |> put_session(:current_user, user)
     |> put_session(:client, client)
-    |> put_session(:notifications, notifications)
     |> redirect(to: "/")
   end
   
-  # TODO look into this more
   defp get_user!(client) do
     {:ok, %{body: user}} = OAuth2.Client.get(client, "/user")
     %{name: user["name"], avatar: user["avatar_url"], username: user["login"]}
-  end
-
-  defp get_notifications!(client) do
-    {:ok, %{body: notifications}} = OAuth2.Client.get(client, "/notifications")
-    notifications
   end
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,19 +2,25 @@ defmodule DidICommit.PageController do
   use DidICommit.Web, :controller
 
   def index(conn, _params) do
-#    IO.puts conn.assigns.current_user.username
     result = if conn.assigns.current_user do
+      today = Timex.now
+      #TODO need to get timezone info from client
+      timezone = Timex.Timezone.get("America/Chicago", today)
+      today = Timex.Timezone.convert(today, timezone)
+      today = Timex.beginning_of_day(today)
+      tomorrow = Timex.end_of_day(today)
+
+      # IO.inspect today
+      # IO.inspect tomorrow
+
       {:ok, %{body: events}} = OAuth2.Client.get(get_session(conn, :client), "/users/" <> conn.assigns.current_user.username <> "/events")
       events
+      |> Enum.filter(fn(%{"type" => type, "created_at" => created_at}) ->	
+	created_at = Timex.Timezone.convert(Timex.parse!(created_at, "{ISO:Extended}"), timezone)
+	type == "PushEvent" && Timex.between?(created_at, today, tomorrow)
+      end)
     end
-
-    result = result
-    |> Enum.filter(fn(%{"type" => type, "created_at" => created_at}) ->
-      type == "PushEvent" &&
-	Timex.Interval.new(from: Timex.now, until: [days: 1])
-	|> Enum.member?(Timex.parse!(created_at, "{ISO:Extended}"))
-    end)
-    IO.inspect result
-    render conn, "index.html", events: result
+    
+    render conn, "index.html", commit: !Enum.empty?(result || [])
   end
 end
