aba3e00a5a494f110100c20b8606bd75fbe54052
Ryan Mulcahy <ryan@primeacademy.io>
2016-08-29 03:00:03 -0500

    More prod fixes.

--- config/prod.exs
+++ config/prod.exs
@@ -64,7 +64,3 @@ config :did_i_commit, GitHub,
   client_id: System.get_env("GITHUB_CLIENT_ID"),
   client_secret: System.get_env("GITHUB_CLIENT_SECRET"),
   redirect_uri: System.get_env("GITHUB_REDIRECT_URI")
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
